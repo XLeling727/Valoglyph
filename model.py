ecf26de3d05e56e1aad63abb27d5a0c77102548c
TheNoZER0 <hypersonic3692@gmail.com>
2024-08-17 16:38:10 +1000

    Added another dataset

--- model.py
+++ model.py
@@ -6,64 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
-
-TEAMS = {"MIBR": 0, 
-         "LeviatÃ¡n": 1, 
-         "Sentinels": 2, 
-         "NRG Esports": 3, 
-         "FURIA": 4, 
-         "100 Thieves": 5, 
-         "LOUD" : 6, 
-         "Evil Geniuses" : 7, 
-         "G2 Esports" : 8, 
-         "Cloud9" : 9, 
-         "KRÃœ Esports" : 10, 
-         "Titan Esports Club" : 11, 
-         "JDG Esports" : 12, 
-         "All Gamers" : 13, 
-         "TYLOO" : 14, 
-         "Bilibili Gaming" : 15, 
-         "Wolves Esports" : 16, 
-         "Dragon Ranger Gaming" : 17, 
-         "Nova Esports" : 18, 
-         "FunPlus Phoenix" : 19, 
-         "Trace Esports" : 20, 
-         "EDward Gaming" : 21, 
-         "ZETA DIVISION" : 22, 
-         "DRX" : 23, 
-         "Team Secret" : 24, 
-         "BLEED" : 25, 
-         "T1" : 26, 
-         "Gen.G" : 27, 
-         "Paper Rex" : 28, 
-         "Talon Esports" : 29, 
-         "Rex Regum Qeon" : 30, 
-         "DetonatioN FocusMe" : 31, 
-         "Global Esports" : 32, 
-         "FUT Esports" : 33, 
-         "KOI" : 34, 
-         "BBL Esports" : 35, 
-         "FNATIC" : 36, 
-         "Team Heretics" : 37, 
-         "Natus Vincere" : 38, 
-         "GIANTX" : 39, 
-         "Gentle Mates" : 40, 
-         "Team Vitality" : 41, 
-         "Team Liquid" : 42, 
-         "Karmine Corp": 43}
-
-MAPS = {"Bind" : 0, 
-        "Haven" : 1, 
-        "Split" : 2, 
-        "Ascent" : 3,
-        "Icebox" : 4, 
-        "Breeze" : 5, 
-        "Fracture" : 6, 
-        "Abyss" : 7, 
-        "Lotus" : 8, 
-        "Sunset" : 9, 
-        "Pearl" : 10}
+from constants import MAPS, TEAMS
 
 data1 = np.genfromtxt("maps_scores.csv", delimiter=',', dtype=None)
 data = data1[1:, 4:15]
